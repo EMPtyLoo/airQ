9881dc4d81a79716671ddf0d146ffa9c36e33cc3
EMPtyLoo <457607792@qq.com>
2015-12-22 16:41:40 +0800

    Initial Commit

---
+++ airQ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  airQ
+//
+//  Created by EMPty on 12/22/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ airQ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  airQ
+//
+//  Created by EMPty on 12/22/15.
+//  Copyright (c) 2015 EMPty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
